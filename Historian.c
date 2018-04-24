9a360798173284ffe7da2324d586e03c0acf7028
Thomas <tseabolt19@gmail.com>
2018-04-24 11:09:31 -0500

    Added Historian.c

---
+++ Historian.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <time.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+int main(int argc, vhar *argv[]) {
+	if (argc < 2){                              //if no port given
+	        printf("usage: %s port\n", argv[0]);     
+	        exit(0);
+	    }
+
+}
